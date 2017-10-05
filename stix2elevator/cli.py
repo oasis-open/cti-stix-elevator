2b3b92f9c9aa0e7bb9347c0d8d4357473e69c90d
Richard Piazza <rpiazza@mitre.org>
2017-10-05 09:47:26 -0400

    remove recursion limit stuff

--- stix2elevator/cli.py
+++ stix2elevator/cli.py
@@ -170,8 +170,6 @@ def main():
     elevator_args = elevator_parser.parse_args()
 
     initialize_options(elevator_args)
-    print(sys.getrecursionlimit())
-    sys.setrecursionlimit(5000)
     result = elevate_file(elevator_args.file_)
     if result:
         print(result + "\n")
