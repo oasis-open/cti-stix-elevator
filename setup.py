baf5e1a94567ab279de32328be6f1b55f1003d25
Greg Back <gback@mitre.org>
2018-03-21 13:00:37 -0500

    Bump to latest stixmarx.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'pycountry>=1.17.8',
         'stix>=1.1.1.9,<1.2.1.0',
         'stix2-validator>=0.1.0',
-        'stixmarx>=1.0.1',
+        'stixmarx>=1.0.3',
         'six>=1.10.0',
         'stix2'
     ],
