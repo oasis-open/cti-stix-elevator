7c7e062e4471d8886f6b43a0fe4ac210d05073ce
Rich Piazza <rpiazza@mitre.org>
2020-01-23 09:16:35 -0500

    in stepper - handle references generically, deprecated removed

--- stix2elevator/stix_stepper.py
+++ stix2elevator/stix_stepper.py
@@ -6,9 +6,7 @@ import sys
 from six import text_type
 from stix2.pattern_visitor import create_pattern_object
 
-from stix2elevator.convert_stix import create_relationship
 from stix2elevator.ids import generate_sco_id
-from stix2elevator.utils import Environment
 
 
 def lookup_stix_id(obs_id, all_objects):
@@ -16,33 +14,28 @@ def lookup_stix_id(obs_id, all_objects):
         return all_objects[obs_id]["id"]
 
 
-def fix_refs_property(obj, refs_property_name, all_objects):
-    refs = obj[refs_property_name]
-    obj[refs_property_name] = []
-    for r in refs:
-        obj[refs_property_name].append(lookup_stix_id(r, all_objects))
-
-
-def fix_all_refs_properties(obj, refs_property_names, all_objects):
-    for name in refs_property_names:
-        if name in obj:
-            fix_refs_property(obj, name, all_objects)
-
-
-def fix_ref_property(obj, property_name, all_objects):
-    obj[property_name] = lookup_stix_id(obj[property_name], all_objects)
-
-
-def fix_all_ref_properties(obj, property_names, all_objects):
-    for name in property_names:
-        if name in obj:
-            fix_ref_property(obj, name, all_objects)
+def handle_references(obj, all_objects):
+    for key, value in obj.items():
+        if isinstance(value, dict):
+            handle_references(value, all_objects)
+        # handle list of objects
+        if isinstance(value, list) and isinstance(value[0], dict):
+            for v in value:
+                handle_references(v, all_objects)
+        elif key.endswith("_ref"):
+            obj[key] = lookup_stix_id(obj[key], all_objects)
+        elif key.endswith("_refs"):
+            refs = obj[key]
+            obj[key] = []
+            for r in refs:
+                obj[key].append(lookup_stix_id(r, all_objects))
 
 
 def step_cyber_observable(obj, observed_data):
     all_objects = observed_data["objects"]
     objs = []
     type_name20 = obj["type"]
+
     if type_name20 == "directory":
         if "created" in obj:
             obj["ctime"] = obj["created"]
@@ -53,18 +46,6 @@ def step_cyber_observable(obj, observed_data):
         if "associated" in obj:
             obj["atime"] = obj["associated"]
             obj.pop("associated", None)
-        if "contains_refs" in obj:
-            fix_refs_property(obj, "contains_refs", all_objects)
-    elif type_name20 == "email-addr":
-        if "belongs_to_ref" in obj:
-            fix_ref_property(obj, "belongs_to_ref", all_objects)
-    elif type_name20 == "email-message":
-        fix_all_ref_properties(obj, ["from_ref", "sender_ref", "raw_email_ref"], all_objects)
-        fix_all_refs_properties(obj, ["to_refs", "cc_refs", "bcc_refs"], all_objects)
-        if "body_multipart" in obj:
-            for mime_part in obj["body_multipart"]:
-                if "body_raw_ref" in mime_part:
-                    fix_ref_property(mime_part, "body_raw_ref", all_objects)
     elif type_name20 == "file":
         obj.pop("is_encrypted", None)
         obj.pop("encryption_algorithm", None)
@@ -80,83 +61,42 @@ def step_cyber_observable(obj, observed_data):
             obj.pop("associated", None)
         if "extensions" in obj:
             exts = obj["extensions"]
-            if "archive-ext" in exts:
-                exts["archive-ext"].pop("version", None)
-                if "contains_refs" in exts["archive-ext"]:
-                    fix_refs_property(exts["archive-ext"], "contains_refs", all_objects)
             if "raster-image-ext" in exts:
                 exts["raster-image-ext"].pop("image_compression_algorithm", None)
-        if "parent_directory_ref" in obj:
-            obj["parent_directory_ref"] = lookup_stix_id(obj["parent_directory_ref"], all_objects)
-        if "contains_refs" in obj:
-            fix_refs_property(obj, "contains_refs", all_objects)
-        if "content_ref" in obj:
-            fix_ref_property(obj, "contains_ref", all_objects)
-    elif type_name20 == 'ipv4-addr' or type_name20 == 'ipv6-addr' or type_name20 == "domain-name":
-        env = Environment(observed_data["created_by_ref"] if "created_by_ref" in observed_data else None,
-                          observed_data["created"])
-        if "resolves_to_refs" in obj:
-            for obs_id in obj["resolves_to_refs"]:
-                objs.append(
-                    create_relationship(obj["id"], lookup_stix_id(obs_id, all_objects), env, "resolves_to"))
-            obj.pop("resolves_to_refs")
-        if "belongs_to_refs" in obj:
-            for obs_id in obj["resolves_to_refs"]:
-                objs.append(
-                    create_relationship(obj["id"], lookup_stix_id(obs_id, all_objects), env, "belongs_to"))
-            obj.pop("belongs_to_refs")
     elif type_name20 == "network-traffic":
-        fix_all_ref_properties(obj,
-                               ["src_ref", "dst_ref", "src_payload_ref", "dst_payload_ref", "encapsulated_by_ref"],
-                               all_objects)
-        if "encapsulates_refs" in obj:
-            fix_refs_property(obj, "encapsulates_refs", all_objects)
         if "extensions" in obj:
             exts = obj["extensions"]
             if "socket-ext" in exts:
                 exts["socket-ext"].pop("protocol_family", None)
-            if "http-request-ext" in exts:
-                if "message_body_data_ref" in exts["http-request-ext"]:
-                    fix_ref_property(exts["http-request-ext"], "message_body_data_ref", all_objects)
     elif type_name20 == "process":
         obj.pop("name", None)
         obj.pop("arguments", None)
         if "binary_ref" in obj:
             obj["image_ref"] = obj["binary_ref"]
             obj.pop("binary_ref", None)
-        fix_all_ref_properties(obj, ["creator_user_ref", "image_ref", "parent_ref"], all_objects)
-        fix_all_refs_properties(obj, ["opened_connection_refs", "child_refs"], all_objects)
-        if "extensions" in obj:
-            exts = obj["extensions"]
-            if "windows-service-ext" in exts:
-                if "service_dll_refs" in exts["windows-service-ext"]:
-                    fix_refs_property(exts["windows-service-ext"], "service_dll_refs", all_objects)
     elif type_name20 == "user-account":
         if "password_last_changed" in obj:
             obj["credential_last_changed"] = obj["password_last_changed"]
             obj.pop("password_last_changed", None)
-    elif type_name20 == "windows-registry-key":
-        if "creator_user_ref" in obj:
-            fix_ref_property(obj, "creator_user_ref", all_objects)
+    handle_references(obj, all_objects)
     objs.append(obj)
     return objs
 
 
-def step_observable_data(object):
-    for key, obj in object["objects"].items():
+def step_observable_data(sco):
+    for key, obj in sco["objects"].items():
         obj["id"] = generate_sco_id(obj["type"], obj)
     objs = list()
-    for key, obj in object["objects"].items():
-        objs.extend(step_cyber_observable(obj, object))
-    object.pop("objects")
-    object["object_refs"] = []
+    for key, obj in sco["objects"].items():
+        objs.extend(step_cyber_observable(obj, sco))
+    sco.pop("objects")
+    sco["object_refs"] = []
     for obj in objs:
-        object["object_refs"].append(obj["id"])
+        sco["object_refs"].append(obj["id"])
         for prop in ["description", "external_references"]:
             if prop in obj and obj[prop] in ("", u"", [], None, dict()):
                 obj.pop(prop)
-    objs.append(object)
-
+    objs.append(sco)
     return objs
 
 
@@ -166,23 +106,21 @@ def step_pattern(pattern):
     return text_type(pattern_obj.toSTIX21())
 
 
-def step_object(object):
-    object["spec_version"] = "2.1"
-    if (object["type"] == "indicator" or object["type"] == "malware" or
-            object["type"] == "report" or object["type"] == "threat-actor" or
-            object["type"] == "tool"):
-        if "labels" in object:
-            types_property_name = object["type"].replace("-", "_") + "_types"
-            object[types_property_name] = object["labels"]
-            object.pop("labels")
-        if object["type"] == "indicator":
-            object["pattern"] = step_pattern(object["pattern"])
-        return [object]
-    elif object["type"] == "observed-data":
-        x = step_observable_data(object)
+def step_object(stix_object):
+    stix_object["spec_version"] = "2.1"
+    if stix_object["type"] in ["indicator", "malware", "report", "threat-actor", "tool"]:
+        if "labels" in stix_object:
+            types_property_name = stix_object["type"].replace("-", "_") + "_types"
+            stix_object[types_property_name] = stix_object["labels"]
+            stix_object.pop("labels")
+        if stix_object["type"] == "indicator":
+            stix_object["pattern"] = step_pattern(stix_object["pattern"])
+        return [stix_object]
+    elif stix_object["type"] == "observed-data":
+        x = step_observable_data(stix_object)
         return x
     else:
-        return [object]
+        return [stix_object]
 
 
 # update "in place"
