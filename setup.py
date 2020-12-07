1b0dd90458fbbb17131bc3a5d055a97fde03a61d
Emmanuelle Vargas-Gonzalez <emmanvg@users.noreply.github.com>
2020-12-07 14:19:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
         'pycountry>=19.8.18',
         'netaddr',
         'stix>=1.1.1.9,<1.2.1.0',
-        'stix2-validator>=2.1.0',
-        'stixmarx>=1.0.7',
+        'stix2-validator>=2.0.2',
+        'stixmarx>=1.0.8',
         'six>=1.12.0',
-        'stix2'
+        'stix2>=2.1.0'
     ],
     entry_points={
         'console_scripts': [
